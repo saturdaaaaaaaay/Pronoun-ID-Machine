4589e8aca4a6eb39f1d94c3724ef3b49ac25bee4
Elie Carlos <ecc226@nau.edu>
2024-04-23 22:50:48 -0700

    Add head menu

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -10,11 +10,15 @@ var skin_features = []
 func _ready():
 	get_tree().get_root().set_transparent_background(true)
 	_get_skin_features()
-	get_node("Color Squares/Color Square 1").connect("item_clicked", self, "_clicked")
-	get_node("Color Squares/Color Square 2").connect("item_clicked", self, "_clicked")
-	get_node("Color Squares/Download").connect("item_clicked", self, "Download_File")
+	get_node("Head Menu/Color Squares/Color Square 1").connect("item_clicked", self, "_switch_color")
+	get_node("Head Menu/Color Squares/Color Square 2").connect("item_clicked", self, "_switch_color")
+	get_node("Head Menu/Head Options/Diamond Head Option").connect("item_clicked", self, "_switch_head")
+	get_node("Head Menu/Head Options/Square Head Option").connect("item_clicked", self, "_switch_head")
+	get_node("Head Menu/Head Options/Wider Head Option").connect("item_clicked", self, "_switch_head")
 	
-func _clicked(path):
+	get_node("Head Menu/Color Squares/Download").connect("item_clicked", self, "_download_file")
+	
+func _switch_color(path):
 	var color = get_node(path).get_self_modulate()
 	
 	get_node("Avatar Image/Heads/Wider Head").set_self_modulate(color)
@@ -23,12 +27,23 @@ func _clicked(path):
 	get_node("Avatar Image/Ears").set_self_modulate(color)
 	get_node("Avatar Image/Torso").set_self_modulate(color)
 	
+func _switch_head(path):
+	get_node("Avatar Image/Heads/Diamond Head").hide()
+	get_node("Avatar Image/Heads/Square Head").hide()
+	get_node("Avatar Image/Heads/Wider Head").hide()
+	if get_node(path) == get_node("Head Menu/Head Options/Diamond Head Option"):
+		get_node("Avatar Image/Heads/Diamond Head").show()
+	if get_node(path) == get_node("Head Menu/Head Options/Square Head Option"):
+		get_node("Avatar Image/Heads/Square Head").show()
+	if get_node(path) == get_node("Head Menu/Head Options/Wider Head Option"):
+		get_node("Avatar Image/Heads/Wider Head").show()
+	
 func _get_skin_features():
 	for child in get_children():
 		print(child)
 		print( child.is_class("Skin Feature"))
 		
-func Download_File(path):
+func _download_file(path):
 	var position = get_node("Avatar Rectangle").rect_position
 	var size = get_node("Avatar Rectangle").rect_size
 	var rect_crop = Rect2(position, size)
